70c4706f922e4711e253a94d2a41c9bd0a7ff598
Vinit Majethiya <vinitmajethiya@gmail.com>
2025-07-28 22:49:54 +0530

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,1 +1,10 @@
-
+def add(a,b):
+    """
+    Adds two numbers together and returns the result.
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+    Returns:
+    int: Sum of a and b.
+    """
+    return a + b
